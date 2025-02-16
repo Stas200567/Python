1a1ddb4b82ea016b9669d1244b795745f42e6e03
Stas200567 <grusastas65@gmail.com>
2025-02-16 12:42:15 +0200

    Перемістив файли у папку LAB 1

--- 1.py
+++
@@ -1,6 +0,0 @@
-def age_to_days(age):
-    return age * 365
-
-print("Вік у днях:", age_to_days(20))
-
-
---
+++ LAB 1/1.py
@@ -0,0 +1,6 @@
+def age_to_days(age):
+    return age * 365
+
+print("Вік у днях:", age_to_days(20))
+
+
